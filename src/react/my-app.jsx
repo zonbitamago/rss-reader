c7f5cdf9a4dbb38f25ede88474d04032ea75d8ee
kazuma takiuchi <basstrombone413@gmail.com>
2017-07-26 23:20:21 +0900

    fix front design

--- src/react/my-app.jsx
+++ src/react/my-app.jsx
@@ -18,14 +18,22 @@ class MyApp extends React.Component {
       var rssSite = items.link.substr(items.link.indexOf('u=') + 2);
       var domain = rssSite.split('/')[2];
       var favicon_url = "http://www.google.com/s2/favicons?domain=" + domain;
+      var date = new Date(items.created).toString();
+      date=date.substr(0,date.indexOf('GMT'));
       var openBrowser = function() {
         //メインプロセスへ送信する
         require("electron").ipcRenderer.send('openBrowser', url);
       };
       return (
         <article className='panel' key={idx}>
-          <div className='favicon'>
-            <img src={favicon_url}/>
+          <div className='head'>
+            <img className='site_icon' src={favicon_url}/>
+            <div className='site_info'>
+              {domain}
+            </div>
+            <div className='date'>
+              {date}
+            </div>
           </div>
           <div className='main'>
             <a href="javascript:void(0)" onClick={openBrowser} target="_brank">{items.title}</a>
@@ -58,7 +66,7 @@ class MyApp extends React.Component {
       });
     }
     load();
-    setInterval(load, 60000);
+    setInterval(load, 1000 * 60 * 5);
   }
 }
 
