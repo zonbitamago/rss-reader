2c1ef13c098ee1aa112b37fee0f438e6cb278bd5
Kazuma Takiuchi <kazuma_takiuchi@scsk-hokkaido.co.jp>
2017-07-28 19:15:07 +0900

    fix react components

---
+++ src/react/itemNodes.jsx
@@ -0,0 +1,44 @@
+const React = require("react");
+
+class ItemNodes extends React.Component {
+  constructor(props) {
+    super(props);
+  };
+  render() {
+      var nodes = this.props.data.map(function(items, idx) {
+      var url = items.link;
+      var rssSite = items.link.substr(items.link.indexOf('u=') + 2);
+      var domain = rssSite.split('/')[2];
+      var favicon_url = "http://www.google.com/s2/favicons?domain=" + domain;
+      var date = new Date(items.created).toString();
+      date = date.substr(0, date.indexOf('GMT'));
+      var openBrowser = function() {
+        //メインプロセスへ送信する
+        require("electron").ipcRenderer.send('openBrowser', url);
+      };
+      return (
+        <article className='panel' key={idx}>
+          <header className='head'>
+            <img className='site_icon' src={favicon_url}/>
+            <div className='site_info'>
+              {domain}
+            </div>
+            <div className='date'>
+              {date}
+            </div>
+          </header>
+          <main className='main'>
+            <a href="javascript:void(0)" onClick={openBrowser} target="_brank">{items.title}</a>
+          </main>
+        </article>
+      )
+    });
+    return (
+      <main className='body-content'>
+      {nodes}
+      </main>
+    );
+  }
+}
+
+module.exports = ItemNodes
--- src/react/my-app.jsx
+++ src/react/my-app.jsx
@@ -3,6 +3,7 @@
 const React = require("react");
 const feed = require('rss-to-json');
 const HomeIcon = require('react-icons/lib/ti/home-outline');
+const ItemNodes = require('./itemNodes');
 const $ = require("jquery");
 let url;
 
@@ -15,34 +16,6 @@ class MyApp extends React.Component {
     };
   }
   render() {
-    var itemNodes = this.state.data.map(function(items, idx) {
-      var url = items.link;
-      var rssSite = items.link.substr(items.link.indexOf('u=') + 2);
-      var domain = rssSite.split('/')[2];
-      var favicon_url = "http://www.google.com/s2/favicons?domain=" + domain;
-      var date = new Date(items.created).toString();
-      date = date.substr(0, date.indexOf('GMT'));
-      var openBrowser = function() {
-        //メインプロセスへ送信する
-        require("electron").ipcRenderer.send('openBrowser', url);
-      };
-      return (
-        <article className='panel' key={idx}>
-          <header className='head'>
-            <img className='site_icon' src={favicon_url}/>
-            <div className='site_info'>
-              {domain}
-            </div>
-            <div className='date'>
-              {date}
-            </div>
-          </header>
-          <main className='main'>
-            <a href="javascript:void(0)" onClick={openBrowser} target="_brank">{items.title}</a>
-          </main>
-        </article>
-      )
-    });
     var scrollTop = function() {
       $('html, body').animate({scrollTop: 0});
     }
@@ -55,9 +28,7 @@ class MyApp extends React.Component {
             <h1>Rss-Reader in React-Electron</h1>
           </div>
         </header>
-        <main className='body-content'>
-          {itemNodes}
-        </main>
+        <ItemNodes url={url} data={this.state.data}/>
       </div>
     );
   }
