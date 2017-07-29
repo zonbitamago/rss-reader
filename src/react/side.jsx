b438830efa22b0721d46de71a9cab8a68e6827bb
kazuma takiuchi <basstrombone413@gmail.com>
2017-07-29 13:31:46 +0900

    fix sidebar

--- src/react/side.jsx
+++ src/react/side.jsx
@@ -1,6 +1,5 @@
 const React = require("react");
 const RSS = require('react-icons/lib/ti/rss');
-const Tooltip = require('rc-tooltip');
 
 class Side extends React.Component {
   constructor(props) {
