0e14a2d0e0f406f5e7085ebf78534fe98e92d6e0
kazuma takiuchi <basstrombone413@gmail.com>
2017-07-30 06:54:58 +0900

    fix sidebar

--- src/react/side.jsx
+++ src/react/side.jsx
@@ -1,5 +1,6 @@
 const React = require("react");
 const RSS = require('react-icons/lib/ti/rss');
+const ReactTooltip = require('react-tooltip')
 
 class Side extends React.Component {
   constructor(props) {
@@ -9,7 +10,17 @@ class Side extends React.Component {
     return (
       <aside className='sidebar'>
         <nav>
-          <RSS className='sideicon rss'/>
+          <RSS className='sideicon rss' data-tip data-for='rss' data-event='click focus'/>
+          <ReactTooltip place="right" id='rss' type="light" effect="solid" globalEventOff='click'>
+            <ul>
+              <li>
+                first
+              </li>
+              <li>
+                second
+              </li>
+            </ul>
+          </ReactTooltip>
         </nav>
       </aside>
     );
