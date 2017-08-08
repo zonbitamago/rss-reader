ac2f922f72fc55d51a96df6488a9316301bc76a7
kazuma takiuchi <basstrombone413@gmail.com>
2017-08-09 05:36:28 +0900

    add jest test

---
+++ src/react/__test__/rssList.test.js
@@ -0,0 +1,20 @@
+'use strict'
+
+jest.unmock('../rssList.jsx')
+
+const React = require('react');
+const ReactDom = require('react-dom');
+const TestUtils = require('react-dom/test-utils');
+const RssList = require('../rssList.jsx');
+
+test('rssList', function() {
+  function setup() {
+    const instance = TestUtils.renderIntoDocument(
+      <RssList />
+    );
+    const rssListNode = ReactDOM.findDOMNode(instance);
+    return {instance, rssListNode};
+
+    expect(true).to.eq(true);
+  }
+});
