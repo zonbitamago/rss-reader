260e4ed3023a9e0c78fae5a1c41c4557055db833
kazuma takiuchi <basstrombone413@gmail.com>
2017-08-10 08:25:37 +0900

    add menu template

--- src/index.js
+++ src/index.js
@@ -47,54 +47,110 @@ function initMenu() {
   // Create the Application's main menu
   var template = [
     {
-      label: "Application",
+      label: 'Edit',
       submenu: [
         {
-          label: "About Application",
-          selector: "orderFrontStandardAboutPanel:"
-        }, {
-          type: "separator"
-        }, {
-          label: "Quit",
-          accelerator: "Command+Q",
-          click: function() {
-            app.quit();
+          label: 'Undo',
+          accelerator: 'CmdOrCtrl+Z',
+          role: 'undo'
+        },
+        {
+          label: 'Redo',
+          accelerator: 'Shift+CmdOrCtrl+Z',
+          role: 'redo'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Cut',
+          accelerator: 'CmdOrCtrl+X',
+          role: 'cut'
+        },
+        {
+          label: 'Copy',
+          accelerator: 'CmdOrCtrl+C',
+          role: 'copy'
+        },
+        {
+          label: 'Paste',
+          accelerator: 'CmdOrCtrl+V',
+          role: 'paste'
+        },
+        {
+          label: 'Select All',
+          accelerator: 'CmdOrCtrl+A',
+          role: 'selectall'
+        }
+      ]
+    },
+    {
+      label: 'View',
+      submenu: [
+        {
+          label: 'Reload',
+          accelerator: 'CmdOrCtrl+R',
+          click: function (item, focusedWindow) {
+            if (focusedWindow) focusedWindow.reload()
+          }
+        },
+        {
+          label: 'Toggle Full Screen',
+          accelerator: (function () {
+            if (process.platform === 'darwin') {
+              return 'Ctrl+Command+F'
+            } else {
+              return 'F11'
+            }
+          })(),
+          click: function (item, focusedWindow) {
+            if (focusedWindow) {
+              focusedWindow.setFullScreen(!focusedWindow.isFullScreen())
+            }
+          }
+        },
+        {
+          label: 'Toggle Developer Tools',
+          accelerator: (function () {
+            if (process.platform === 'darwin') {
+              return 'Alt+Command+I'
+            } else {
+              return 'Ctrl+Shift+I'
+            }
+          })(),
+          click: function (item, focusedWindow) {
+            if (focusedWindow) focusedWindow.toggleDevTools()
           }
         }
       ]
-    }, {
-      label: "Edit",
+    },
+    {
+      label: 'Window',
+      role: 'window',
       submenu: [
         {
-          label: "Undo",
-          accelerator: "CmdOrCtrl+Z",
-          selector: "undo:"
-        }, {
-          label: "Redo",
-          accelerator: "Shift+CmdOrCtrl+Z",
-          selector: "redo:"
-        }, {
-          type: "separator"
-        }, {
-          label: "Cut",
-          accelerator: "CmdOrCtrl+X",
-          selector: "cut:"
-        }, {
-          label: "Copy",
-          accelerator: "CmdOrCtrl+C",
-          selector: "copy:"
-        }, {
-          label: "Paste",
-          accelerator: "CmdOrCtrl+V",
-          selector: "paste:"
-        }, {
-          label: "Select All",
-          accelerator: "CmdOrCtrl+A",
-          selector: "selectAll:"
+          label: 'Minimize',
+          accelerator: 'CmdOrCtrl+M',
+          role: 'minimize'
+        },
+        {
+          label: 'Close',
+          accelerator: 'CmdOrCtrl+W',
+          role: 'close'
+        }
+      ]
+    },
+    {
+      label: 'Help',
+      role: 'help',
+      submenu: [
+        {
+          label: 'Learn More',
+          click: function () { require('electron').shell.openExternal('https://electron.atom.io') }
         }
       ]
     }
-  ];
+  ]
 
   Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 }
