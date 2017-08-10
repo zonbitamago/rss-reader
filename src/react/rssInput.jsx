a9208de8cdb7f901747a9bc2fa0c8b0d10f96011
Kazuma Takiuchi <kazuma_takiuchi@scsk-hokkaido.co.jp>
2017-08-10 20:01:03 +0900

    fix add urls

--- src/react/rssInput.jsx
+++ src/react/rssInput.jsx
@@ -2,6 +2,7 @@ const React = require("react");
 const fs = require("fs");
 const path = require("path");
 const info_path = path.join(require('electron').remote.app.getPath("userData"), "./urlList.json");
+const feed = require('rss-to-json');
 let set = new Set();
 
 class RssInput extends React.Component {
@@ -17,11 +18,11 @@ class RssInput extends React.Component {
     this.setURLContent = this.setURLContent.bind(this);
   };
   changeName(e) {
-    this.setState({name:e.target.value});
+    this.setState({name: e.target.value});
   };
 
   changeURL(e) {
-    this.setState({url:e.target.value});
+    this.setState({url: e.target.value});
   };
 
   setURLContent() {
@@ -44,15 +45,37 @@ class RssInput extends React.Component {
   };
 
   save() {
-    this.setURLContent();
-
-    console.log('set:', this.props.set);
-    fs.writeFileSync(info_path, JSON.stringify([...this.props.set]), function(err) {
-      console.log('err');
-      console.log(err);
+    var state = this;
+    var promise = new Promise(function(resolve, reject) {
+      feed.load(state.state.url, function(err, rss) {
+        console.log('feed');
+        if (!err) {
+          resolve(rss);
+        } else {
+          reject(err);
+        }
+      });
+      console.log('process');
+      console.log(feed);
     });
-    this.props.parent.forceUpdate();
-    console.log('save');
+
+    promise.then(function(rss) {
+      state.setURLContent();
+
+      console.log('set:', state.props.set);
+      fs.writeFileSync(info_path, JSON.stringify([...state.props.set]), function(err) {
+        console.log('err');
+        console.log(err);
+      });
+      state.props.parent.forceUpdate();
+      console.log('save');
+
+    }, function(err) {
+      console.log('err:');
+      console.log(err);
+      alert('登録できないURLです。');
+    });
+
   };
   render() {
     return (
