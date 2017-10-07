4b8c62cb29d58f0fc5ba3f0eb1827758b197e253
kazuma takiuchi <basstrombone413@gmail.com>
2017-10-08 05:51:54 +0900

    add action test

---
+++ v2/src/react/__test__/actions/index.test.js
@@ -0,0 +1,17 @@
+'use strict';
+jest.dontMock('../../actions/index');
+jest.dontMock('../../utils/actionTypes');
+import * as actions from '../../actions/index';
+import * as actionTypes from '../../utils/actionTypes';
+
+test('onShutDownClick', () => {
+  expect(actions.onShutDownClick()).toEqual({type: actionTypes.SHUTDOWN});
+});
+
+test('onMimizeClick', () => {
+  expect(actions.onMimizeClick()).toEqual({type: actionTypes.MINIMIZE});
+});
+
+test('onOpenGithubClick', () => {
+  expect(actions.onOpenGithubClick()).toEqual({type: actionTypes.OPEN_GITHUB});
+});
