7654cb4ab1ac3a860a03f0d6bd468df4583c082f
kazuma takiuchi <basstrombone413@gmail.com>
2017-10-14 07:52:51 +0900

    fix rssListModal CloseBtn

--- v2/src/react/__test__/components/rssListModal.test.js
+++ v2/src/react/__test__/components/rssListModal.test.js
@@ -2,14 +2,22 @@
 jest.dontMock('../../components/rssListModal.jsx');
 import React from 'react';
 import renderer from 'react-test-renderer';
+import {mount} from 'enzyme';
 import RssListModal from '../../components/rssListModal.jsx';
 
 const open = false;
+const onRssListModalClick = jest.fn();
 const actions = {
-  onRssListModalClick: jest.fn()
+  onRssListModalClick: onRssListModalClick
 };
 
 test('snapshot', function() {
   const tree = renderer.create(<RssListModal actions={actions} open={open}/>).toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+test('dummy',() => {
+   const dummy = mount(<RssListModal actions={actions} open={true}/>);
+   dummy.simulate('click');
+   expect(onRssListModalClick).toHaveBeenCalled();
+})
--- v2/src/react/components/rssListModal.jsx
+++ v2/src/react/components/rssListModal.jsx
@@ -1,7 +1,8 @@
 'use strict';
 
 import React from 'react';
-import {Button, Header, Icon, Modal} from 'semantic-ui-react'
+import {Button, Header, Icon, Modal} from 'semantic-ui-react';
+import styles from '../styles/rssListModal.css';
 
 class RssListModal extends React.Component {
   render() {
@@ -10,6 +11,8 @@ class RssListModal extends React.Component {
         open={this.props.open}
         onClose={this.props.actions.onRssListModalClick}
         trigger={<Icon color='yellow' name='rss' />}
+        className={styles.background}
+        mountNode={document.querySelector('section')}
       >
         <Modal.Header>RssList</Modal.Header>
         <Modal.Content scrolling>
@@ -18,7 +21,7 @@ class RssListModal extends React.Component {
           </div>
         </Modal.Content>
         <Modal.Actions>
-          <Button color='red'>
+          <Button color='red' onClick={this.props.actions.onRssListModalClick}>
             <Icon name='remove'/>
             No
           </Button>
