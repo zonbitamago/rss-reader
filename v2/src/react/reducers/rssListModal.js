e98b23fca9c3678d63a5ffb9e8813a12bd11d4d5
kazuma takiuchi <basstrombone413@gmail.com>
2017-10-19 05:08:43 +0900

    fix rssListModal conmponent

--- v2/src/react/reducers/rssListModal.js
+++ v2/src/react/reducers/rssListModal.js
@@ -13,13 +13,65 @@ const rssListModal = (state = initialAppState, action) => {
     };
   } else if (action.type === actionTypes.RSSINPUT) {
     console.log('rssInputClick!');
-    return {
-      state,
-      rssListModalOpen: state.rssListModalOpen
-    };
+    return {state, rssListModalOpen: state.rssListModalOpen};
   } else {
     return state;
   }
 };
 
+const setURLContent = () => {
+  var state = this;
+  if (this.props.set.size == 0) {
+    this.props.set.add(state.state);
+  } else {
+    var exists = false;
+    this.props.set.forEach((val) => {
+      if (val.name == state.state.name) {
+        exists = true;
+        val.url = state.state.url;
+      }
+    });
+
+    if (!exists) {
+      this.props.set.add(state.state);
+    }
+  }
+};
+
+const save = () => {
+  var state = this;
+  var url = state.state.url;
+  var promise = new Promise((resolve, reject) => {
+    var protocol = (url.startsWith('https:')
+      ? https
+      : http);
+    protocol.get(url, (res) => {
+      var parser = new FeedMe(true);
+      res.pipe(parser);
+      parser.on('end', () => {
+        resolve(parser.done());
+      });
+    });
+  });
+
+  promise.then((rss) => {
+    state.setURLContent();
+
+    console.log('set:', state.props.set);
+    fs.writeFileSync(info_path, JSON.stringify([...state.props.set]), function(err) {
+      console.log('err');
+      console.log(err);
+    });
+    state.props.parent.forceUpdate();
+    console.log('save');
+
+    state.props.load();
+
+  }, (err) => {
+    console.log('err:');
+    console.log(err);
+    alert('登録できないURLです。');
+  });
+
+};
 export default rssListModal;
