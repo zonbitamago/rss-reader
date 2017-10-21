67ce161f791baf31f4c4ca556cd10390ac036fe0
kazuma takiuchi <basstrombone413@gmail.com>
2017-10-22 00:21:40 +0900

    fix rssListModal test

--- v2/src/react/__test__/reducers/rssListModal.test.js
+++ v2/src/react/__test__/reducers/rssListModal.test.js
@@ -2,6 +2,7 @@
 jest.dontMock('../../reducers/rssListModal');
 import rssListModal from '../../reducers/rssListModal';
 import * as actionTypes from '../../utils/actionTypes';
+import * as constants from '../../utils/constants';
 
 const initialAppState = {
   rssListModalOpen: false
@@ -12,17 +13,25 @@ test('rssListModal', () => {
     type: actionTypes.RSSLISTMODAL
   };
   var rssListModalAction = rssListModal(initialAppState, action);
-  expect(rssListModalAction).toEqual({rssListModalOpen: true, state: initialAppState});
+  expect(rssListModalAction).toEqual({rssListModalOpen: true, state: initialAppState, urlList: {}});
 })
 
 test('rssInput', () => {
   var action = {
     type: actionTypes.RSSINPUT,
-    name:'name',
-    url:'url'
+    name: 'name',
+    url: 'url'
   };
   var rssListModalAction = rssListModal(initialAppState, action);
-  expect(rssListModalAction).toEqual({rssListModalOpen: false, state: initialAppState});
+  
+  expect(rssListModalAction).toEqual({
+    rssListModalOpen: false,
+    state: initialAppState,
+    urlList: {
+      name: 'name',
+      url: 'url'
+    }
+  });
 })
 
 test('else', () => {
