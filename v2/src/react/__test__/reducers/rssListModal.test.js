459b7c3c188c49432142002f31f99617d13c3855
kazuma takiuchi <basstrombone413@gmail.com>
2017-10-27 00:01:07 +0900

    fix rssListModal

--- v2/src/react/__test__/reducers/rssListModal.test.js
+++ v2/src/react/__test__/reducers/rssListModal.test.js
@@ -1,6 +1,6 @@
 'use strict';
 jest.dontMock('../../reducers/rssListModal');
-import rssListModal from '../../reducers/rssListModal';
+import rssListModal, {save} from '../../reducers/rssListModal';
 import * as actionTypes from '../../utils/actionTypes';
 import * as constants from '../../utils/constants';
 
@@ -16,23 +16,107 @@ test('rssListModal', () => {
   expect(rssListModalAction).toEqual({rssListModalOpen: true, state: initialAppState, urlList: {}});
 })
 
-test('rssInput', () => {
-  var action = {
-    type: actionTypes.RSSINPUT,
-    name: 'name',
-    url: 'url'
-  };
-  var rssListModalAction = rssListModal(initialAppState, action);
-  
-  expect(rssListModalAction).toEqual({
-    rssListModalOpen: false,
-    state: initialAppState,
-    urlList: {
-      name: 'name',
-      url: 'url'
-    }
+// test('rssInput', () => {
+//   var action = {
+//     type: actionTypes.RSSINPUT,
+//     name: 'name',
+//     url: 'url'
+//   };
+//   var rssListModalAction = rssListModal(initialAppState, action);
+//
+//   expect(rssListModalAction).toEqual({
+//     rssListModalOpen: false,
+//     state: initialAppState,
+//     urlList: {
+//       name: 'name',
+//       url: 'url'
+//     }
+//   });
+// })
+
+describe('functions', () => {
+  test('引数を受け取って、rssListを保存する。', () => {
+    localStorage.rssList = undefined;
+    save('name1', 'http://www.feedforall.com/sample-feed.xml');
+    var rssList = JSON.parse(localStorage.rssList);
+    expect(rssList).toEqual([
+      {
+        name: 'name1',
+        url: 'http://www.feedforall.com/sample-feed.xml'
+      }
+    ]);
+
+    localStorage.rssList = undefined;
+    save('name2', 'http://www.feedforall.com/sample.xml');
+    var rssList = JSON.parse(localStorage.rssList);
+    expect(rssList).toEqual([
+      {
+        name: 'name2',
+        url: 'http://www.feedforall.com/sample.xml'
+      }
+    ]);
+
+    localStorage.rssList = undefined;
+    save('name1', 'http://www.feedforall.com/sample-feed.xml');
+    save('name2', 'http://www.feedforall.com/sample.xml');
+    var rssList = JSON.parse(localStorage.rssList);
+    expect(rssList).toEqual([
+      {
+        name: 'name1',
+        url: 'http://www.feedforall.com/sample-feed.xml'
+      }, {
+        name: 'name2',
+        url: 'http://www.feedforall.com/sample.xml'
+      }
+    ]);
+
   });
-})
+
+  test('重複した内容のrssListを保存する', () => {
+    localStorage.rssList = undefined;
+    save('name1', 'http://www.feedforall.com/sample-feed.xml');
+    save('name2', 'http://www.feedforall.com/sample.xml');
+    save('name2', 'http://www.feedforall.com/sample.xml');
+    var rssList = JSON.parse(localStorage.rssList);
+    expect(rssList).toEqual([
+      {
+        name: 'name1',
+        url: 'http://www.feedforall.com/sample-feed.xml'
+      }, {
+        name: 'name2',
+        url: 'http://www.feedforall.com/sample.xml'
+      }
+    ]);
+  });
+
+  test('引数で受け取ったURLが有効かを確認する。', () => {
+    localStorage.rssList = undefined;
+    save('name1', 'http://www.feedforall.com/sample-feed.xml');
+    var rssList = JSON.parse(localStorage.rssList);
+    expect(rssList).toEqual([
+      {
+        name: 'name1',
+        url: 'http://www.feedforall.com/sample-feed.xml'
+      }
+    ]);
+
+    localStorage.rssList = undefined;
+    save('name2', 'NG');
+    expect(localStorage.rssList).toEqual(undefined);
+
+    localStorage.rssList = undefined;
+    save('name1', 'http://www.feedforall.com/sample-feed.xml');
+    save('name2', 'NG');
+    var rssList = JSON.parse(localStorage.rssList);
+    expect(rssList).toEqual([
+      {
+        name: 'name1',
+        url: 'http://www.feedforall.com/sample-feed.xml'
+      }
+    ]);
+
+  });
+});
 
 test('else', () => {
   var action = {
--- v2/src/react/reducers/rssListModal.js
+++ v2/src/react/reducers/rssListModal.js
@@ -22,31 +22,39 @@ const rssListModal = (state = initialAppState, action) => {
     console.log('rssInputClick!');
     var urlList = getUrlList();
     // urlList = setURLContent(urlList, action.name, action.url);
-    urlList = save(urlList, action.name, action.url);
-    return {state, rssListModalOpen: state.rssListModalOpen, urlList: urlList};
+    save(urlList, action.name, action.url);
+    var newUrlList = getUrlList();
+    console.log(newUrlList);
+    return {state, rssListModalOpen: state.rssListModalOpen, urlList: newUrlList};
   } else {
     return state;
   }
+
 };
 
-const setURLContent = (urlList, name, url) => {
-  if (!urlList.length || urlList.length == 0) {
-    return {name: name, url: url};
-  } else {
-    var exists = false;
-    urlList.forEach((val) => {
-      if (val.name == name) {
-        exists = true;
-        val.url = url;
-      }
-    });
-
-    if (!exists) {
-      urlList.add({name: name, url: url});
-    }
-    return urlList;
-  }
-};
+// const setURLContent = (urlList, name, url) => {
+//   if (!urlList.length || urlList.length == 0) {
+//     return [
+//       {
+//         name: name,
+//         url: url
+//       }
+//     ];
+//   } else {
+//     var exists = false;
+//     urlList.forEach((val) => {
+//       if (val.name == name) {
+//         exists = true;
+//         val.url = url;
+//       }
+//     });
+//
+//     if (!exists) {
+//       urlList.push({name: name, url: url});
+//     }
+//     return urlList;
+//   }
+// };
 
 function getUrlList() {
   var urlList = {};
@@ -58,8 +66,84 @@ function getUrlList() {
   return urlList;
 }
 
-const save = (urlList, name, url) => {
-  var state = this;
+// const save = (urlList, name, url) => {
+//   var state = this;
+//   var promise = new Promise((resolve, reject) => {
+//     var protocol = (url.startsWith('https:')
+//       ? https
+//       : http);
+//     protocol.get(url, (res) => {
+//       var parser = new FeedMe(true);
+//       res.pipe(parser);
+//       parser.on('end', () => {
+//         resolve(parser.done());
+//       });
+//     });
+//   });
+//
+//   promise.then((rss) => {
+//     urlList = setURLContent(urlList, name, url);
+//
+//     fs.writeFileSync(constants.info_path, JSON.stringify(urlList), function(err) {
+//       console.log('err');
+//       console.log(err);
+//     });
+//     // state.props.parent.forceUpdate();
+//     console.log('save');
+//
+//     // state.props.load();
+//     return urlList;
+//
+//   }, (err) => {
+//     console.log('err:');
+//     console.log(err);
+//     alert('登録できないURLです。');
+//     return urlList;
+//   });
+//
+// };
+
+export var save = (name, url) => {
+  if (!checkURL(url)) {
+    return;
+  };
+  localStorage.rssList = JSON.stringify(setRssList(name, url));
+};
+
+var setRssList = (name, url) => {
+  if (localStorage.rssList === undefined || localStorage.rssList.length == 0) {
+    var rssList = [
+      {
+        name: name,
+        url: url
+      }
+    ];
+    return rssList;
+  } else {
+    var rssList = setURLContent(JSON.parse(localStorage.rssList), name, url);
+    return rssList;
+  }
+
+};
+
+var setURLContent = (rssList, name, url) => {
+  var exists = false;
+  rssList.forEach((val) => {
+    if (val.name == name) {
+      exists = true;
+      val.url = url;
+    }
+  });
+
+  if (!exists) {
+    rssList.push({name: name, url: url});
+  }
+  return rssList;
+};
+
+var checkURL = (url) => {
+  var isCheck = true;
+
   var promise = new Promise((resolve, reject) => {
     var protocol = (url.startsWith('https:')
       ? https
@@ -74,24 +158,20 @@ const save = (urlList, name, url) => {
   });
 
   promise.then((rss) => {
-    urlList = setURLContent(urlList, name, url);
-
-    fs.writeFileSync(constants.info_path, JSON.stringify(urlList), function(err) {
-      console.log('err');
-      console.log(err);
-    });
-    // state.props.parent.forceUpdate();
-    console.log('save');
-
-    // state.props.load();
-    return urlList;
-
+    // setIsCheck(true);
+    // console.log(2);
+    isCheck = true;
   }, (err) => {
-    console.log('err:');
-    console.log(err);
+    // console.log('err:');
+    // console.log(err);
+    // console.log(2);
+    isCheck = false;
     alert('登録できないURLです。');
-    return urlList;
+    // setIsCheck(false);
   });
-
+  // console.log(promise);
+  // console.log(1);
+  return isCheck;
 };
+
 export default rssListModal;
