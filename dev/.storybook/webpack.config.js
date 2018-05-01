3732a621a2ebbc4ca0b69486d5bc8553f4b1c3f4
kazuma takiuchi <basstrombone413@gmail.com>
2018-05-02 04:57:28 +0900

    fix webpack config

--- dev/.storybook/webpack.config.js
+++ dev/.storybook/webpack.config.js
@@ -1,7 +1,6 @@
 const path = require("path");
 
 module.exports = (baseConfig, env, defaultConfig) => {
-  console.log(defaultConfig);
   // Extend defaultConfig as you need.
 
   defaultConfig.target = "electron-main";
