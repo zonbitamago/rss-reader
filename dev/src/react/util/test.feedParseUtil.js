70cd2b997c8a68d6e45578958dc7a79d57ae65e2
kazuma takiuchi <basstrombone413@gmail.com>
2018-06-01 05:12:32 +0900

    add feedParseUtil test

---
+++ dev/src/react/util/test.feedParseUtil.js
@@ -0,0 +1,11 @@
+import feedParse from "./feedParseUtil";
+
+describe("feedParseUtil", () => {
+  describe("正常系", () => {
+    it("RSS1.0", () => {});
+
+    it("RSS2.0", () => {});
+
+    it("Atom", () => {});
+  });
+});
