75dc724043915661d5f94401de0b1c1577ddcc31
kazuma takiuchi <basstrombone413@gmail.com>
2018-12-14 05:57:38 +0900

    fix time format

--- src/react/util/timeUtil.js
+++ src/react/util/timeUtil.js
@@ -3,8 +3,9 @@ import moment from "moment";
 export function transformDate(timeInMS) {
   var momentDate = moment(timeInMS);
   var momentNow = moment(new Date());
-  if (momentNow.diff(momentDate, "days") > 0) {
-    return momentDate.format("LL");
-  }
-  return momentDate.startOf("minute").fromNow();
+  // if (momentNow.diff(momentDate, "days") > 0) {
+  //   return momentDate.format("YYYY/MM/DD HH:mm:ss");
+  // }
+  // return momentDate.startOf("YYYY/MM/DD HH:mm:ss").fromNow();
+  return momentDate.format("YYYY/MM/DD HH:mm:ss");
 }
